030eff322461700f67221ff04dc4a2c559ff2f6e
mathias <mathias@odlund.no>
2023-04-13 23:02:27 +0200

    Feature. Introduced Adonis

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import '../layout.css';
 	import Navbar from '../components/navbar/navbar.svelte';
+	import Footer from '../components/footer/footer.svelte';
 </script>
 
 <div>
 	<Navbar />
-	<slot />
+	<div class="min-h-screen">
+		<slot />
+	</div>
+	<Footer />
 </div>
 
 <style>
